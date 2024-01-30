a4cf0473eedbe9cc550f02ec2f9a309f1246c046
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-01-30 12:02:56 +0500

    lang\uz\password.php translate

--- lang/uz/passwords.php
+++ lang/uz/passwords.php
@@ -6,17 +6,11 @@ return [
     |--------------------------------------------------------------------------
     | Password Reset Language Lines
     |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
-
+    'reset' => 'Parolingiz qayta tiklandi.',
+    'sent' => 'Parolni tiklash havolasini elektron pochta orqali yubordik.',
+    'throttled' => 'Qayta urinishdan oldin kuting.',
+    'token' => 'Bu parolni tiklash tokeni yaroqsiz.',
+    'user' => "Bunday elektron pochta manziliga ega foydalanuvchini topa olmadik.",
 ];
